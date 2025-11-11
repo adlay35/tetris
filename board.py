3cd153d1b5bcbf94825d7ef70974b1cfed4541ae
adlay <yeachan0305@naver.com>
2025-11-11 17:11:27 +0900

    함수속에 넣은 상수값들 위치 이동

--- board.py
+++ board.py
@@ -1,3 +1,9 @@
+WALL_CHELL = 8
+BOARD_WIDTH = 10
+BOARD_HEIGHT = 20
+TOTAL_COLS = BOARD_WIDTH + 2
+TOTAL_ROWS = BOARD_HEIGHT + 2
+
 def reset_board():
     """게임 보드를 초기화하는 함수.
 
@@ -6,15 +12,11 @@ def reset_board():
     Returns:
         list: 초기화된 게임 보드 값이 담긴 2차원 리스트
     """
-    WALL_CHELL = 8
-    BOARD_WIDTH = 10
-    BOARD_HEIGHT = 20
-    TOTAL_COLS = BOARD_WIDTH + 2
-    TOTAL_ROWS = BOARD_HEIGHT + 2
-
     top_wall = [WALL_CHELL] * TOTAL_COLS
     middle_wall = [WALL_CHELL] + ([0] * BOARD_WIDTH) + [WALL_CHELL]
 
     board = [top_wall] + ([middle_wall] * BOARD_HEIGHT) + [top_wall]
 
     return board
+
+print(reset_board())
