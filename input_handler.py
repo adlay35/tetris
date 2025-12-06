ebb32c18df3facc20c64db4b8836cf7b665d9c37
adlay <yeachan0305@naver.com>
2025-12-06 18:43:22 +0900

    edit?

--- input_handler.py
+++ input_handler.py
@@ -1,1 +1,21 @@
+import curses
+
 # key = window.getkey()
+
+def get_user_input(window):
+    user_input = window.getkey()
+    return user_input
+
+def select_next_calculate(user_input)
+    if key == "KEY_RIGHT" or key == "KEY_LEFT" or key == "KEY_DOWN":
+        # 이동
+    elif key == "KEY_UP":
+        # 오른쪽으로 90도 회전
+    elif key == "z":
+        # 왼쪽으로 90도 회전
+    elif key == " ":
+        # 하드 드랍
+    elif key == "a":
+        # 180도 회전 
+    elif key == "c":
+        # 홀드
